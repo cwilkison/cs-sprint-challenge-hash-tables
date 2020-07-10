c7472bb8773d8adba93bd12a761768931ab29595
Cole Wilkison <wilkison.cole@gmail.com>
2020-07-10 15:27:40 -0400

    mvp sprint hash

--- hashtables/ex3/ex3_tests.py
+++ hashtables/ex3/ex3_tests.py
@@ -53,3 +53,4 @@ class TestEx2(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -43,3 +43,6 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+
+##MVP
